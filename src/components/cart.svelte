7d66bee7f1774beea7808885969587444e59b1c8
Ruchdane AMADOU <ruchdaneabiodun@gmail.com>
2022-12-10 10:52:45 +0100

    update cart style

--- src/components/cart.svelte
+++ src/components/cart.svelte
@@ -1,5 +1,5 @@
 <script>
-    import { cart, removeItem, checkout } from '../stores'
+    import { cart, removeItem, checkout } from "../stores";
 
     const trash = "/icons/icon-delete.svg";
 </script>
@@ -9,8 +9,8 @@
     {#if $cart.items.length > 0}
         <ul class="cart__items">
             {#each $cart.items as { count, name, thumb, unit_price }, index (index)}
-                <li class="cart__item">
-                    <img src={thumb} alt={thumb} class="item_thumb" />
+                <li class="cart__item item">
+                    <img src={thumb} alt={thumb} class="item__thumb" />
                     <div class="item__name">{name}</div>
                     <div class="item__price">
                         <span class="item__price-calculation">
@@ -26,13 +26,91 @@
                     >
                         <img src={trash} alt="delete" />
                     </button>
-                    <button class="item__checkout" on:click={() => checkout()}>
-                        Checkout
-                    </button>
                 </li>
             {/each}
         </ul>
+        <button class="item__checkout" on:click={() => checkout()}>
+            Checkout
+        </button>
     {:else}
         <div class="cart__empty">Your cart is empty</div>
     {/if}
 </div>
+
+<style lang="scss">
+    .cart {
+        position: absolute;
+        z-index: var(--z-cart);
+        top: 82px;
+        left: 60%;
+        overflow-y: auto;
+        background-color: var(--white);
+        box-shadow: 0px 10px 20px 0px var(--grayish-blue);
+        // box-shadow: 0px 10px 20px 0px rgba(0, 0, 0, 0.2); /* adjust the values to add a softer shadow */
+        width: fit-content;
+        min-width: 300px;
+        min-height: 200px;
+        border-radius: 10px;
+        &__title {
+            padding:1rem;
+            font-weight: 700;
+            font-size: 1.2rem;
+            // Border to create a line separation betwwen the title and the items
+            border-bottom: 1px solid var(--dark-grayish-blue);
+        }
+        
+        &__items {
+            padding: 1rem;
+            display: flex;
+            flex-direction: column;
+            list-style-type: none;
+            gap: 10px;
+        }
+        &__empty {
+            text-align: center;
+            vertical-align: middle;
+        }
+    }
+    .item {
+        display: grid;
+        gap: 0.5rem 1rem;
+        grid-template-areas:
+            "thumb name  name  delete"
+            "thumb price price delete";
+
+        &__thumb {
+            grid-area: thumb;
+            border-radius: 10px;
+            max-width: 3rem;
+        }
+        &__name {
+            grid-area: name;
+        }
+        &__price {
+            grid-area: price;
+        }
+
+        &__delete {
+            grid-area: delete;
+            background-color: transparent;
+            border: none;
+            height: fit-content;
+            align-self: center;
+        }
+
+        &__checkout{
+            margin: 1rem;
+            padding: 1rem;
+            width: 100%;
+            background-color: var(--orange);
+            color: var(--white);
+        }
+
+        &__price-calculation {
+        }
+        &__price-value {
+        }
+        &__checkout {
+        }
+    }
+</style>
